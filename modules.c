241b65a68e3095e0d48efc539908a4ef79a5641a
stormlin <376997313@qq.com>
2017-07-16 18:04:06 +0800

    Extracted modules for enabling and disabling

---
+++ modules.c
@@ -0,0 +1,33 @@
+/* Modules for enabling and disabling tso feature */
+
+#include <stdbool.h>
+
+/* Detemine whether the status of tso is changable */
+static bool IsChangable(char *deviceName)
+{
+
+}
+
+/* Enable tso feature in given NIC, return ture if successfully enabled */
+static bool EnableTSO(char *deviceName)
+{
+    /* If it is fixed, return false */
+    if (!IsChangable(deviceName))
+    {
+        return false;
+    }
+
+    /* Try to enable it */
+}
+
+/* Disable tso feature in given NIC, return true if successfully diabled */
+static bool DisableTSO(char *deviceName)
+{
+    /* If it is fixed, return false */
+    if (!IsChangable(deviceName))
+    {
+        return false;
+    }
+
+    /* Try to disable it */
+}
